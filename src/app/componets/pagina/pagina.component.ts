49d143d69421b30cc499df9599005305ea2271f3
axlopezre <83840788+axlopezre@users.noreply.github.com>
2022-08-25 10:37:14 -0600

    prueba final espacio

--- src/app/componets/pagina/pagina.component.ts
+++ src/app/componets/pagina/pagina.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pagina',
   templateUrl: './pagina.component.html',
-  styleUrls: ['./pagina.component.css']
+  styleUrls: ['./pagina.component.css', '/pagina.component2.css']
 })
 export class PaginaComponent implements OnInit {
 
